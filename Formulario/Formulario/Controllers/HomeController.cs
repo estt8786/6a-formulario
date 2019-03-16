eb30ab4f385eb08e02308fa418bcd6e83f3ec5d3
Virgilio Quintino <estt8786@ipt.pt>
2019-03-16 23:16:11 +0000

    Correção do Formulário para requerer o nome e validar a idade.

--- Formulario/Formulario/Controllers/HomeController.cs
+++ Formulario/Formulario/Controllers/HomeController.cs
@@ -14,6 +14,7 @@ namespace Formulario.Controllers
             // responde ao GET porque é o que ele responde por defeito no HTTP...
             // necessario para ter um valor para o razor
             // para inicializar a variavel
+            // inicializar valor inicial do ViewBag
             ViewBag.Resposta = "";
 
             return View();
@@ -22,29 +23,38 @@ namespace Formulario.Controllers
         // POST: Home
         [HttpPost]
         // papel deste metodo, HTTP e faz POST, o que está dentro do []
-        // porcessar os dados do formulario
-        // receber os dados quem do servidor
+        // processar os dados do formulario
+        // receber os dados que vem do servidor
         // criar um break point ---executar e vai para aqui
+        //os tipos primitivos nao podem ser nulos, entao com o int+? pode conter nulos...
         public ActionResult Index(string nome, int? idade)
-            //os tipos primitivos nao podem ser nulos, entao com o int+? pode conter nulos...
         {
+            // Precisamos de validar os dados introduzidos pelo utilizador
+            // 1ª questão: O 'nome' é um nome só em caracteres?
+            // 2ª questão: a idade está dentro dos parametros pretendidos [0; 120]?
+
             // var auxiliar
             string resposta = "";
 
-            if (nome != "") { 
-            //preparar a resposta a enviar para a view
-            resposta = "Você chama-se " + nome + "e tem " + idade + " anos!";
+            //if(nome != "")
+            //{
+            //    // preparar a resposta a enviar para a View
+            //    resposta = "Voce chama-se " + nome;
+            //}
+
+            if(idade >= 0 && idade <= 120){
+                resposta = "Voce chama-se " + nome +" e tem " + idade + " anos!";
+            }
+            else
+            {
+                // mensagem alternativa
+                resposta = "Deve especificar uma idade váilda!\n" +
+                            "A idade deve ser maior que zero e menor que 120 anos...";
             }
 
-            if(idade != null) {
-            resposta = "e tem " + idade + " anos!";
-
-            }
-
+            // criar o 'contentor' que levará a mesnagem para a View
             // enviar a "resposta" para a view
             ViewBag.Resposta = resposta;
-
-            
 
             return View();
         }
